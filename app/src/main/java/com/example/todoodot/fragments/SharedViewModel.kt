1290448f78d72e913dc41a46c6295735daa673cd
Prashant <prashant.patel77@gmail.com>
2021-09-11 21:21:23 +1000

    Added Empty data view functionality

--- app/src/main/java/com/example/todoodot/fragments/SharedViewModel.kt
+++ app/src/main/java/com/example/todoodot/fragments/SharedViewModel.kt
@@ -7,10 +7,18 @@ import android.widget.AdapterView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.example.todoodot.R
 import com.example.todoodot.data.models.Priority
+import com.example.todoodot.data.models.ToDoData
 
 class SharedViewModel(application: Application) : AndroidViewModel(application) {
+
+    //for empty data view
+    val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(true)
+    fun checkIfDatabaseEmpty(data: List<ToDoData>) {
+        emptyDatabase.value = data.isEmpty()
+    }
 
     val spinnerListener: AdapterView.OnItemSelectedListener = object : AdapterView.OnItemSelectedListener{
         override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
--- app/src/main/java/com/example/todoodot/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoodot/fragments/list/ListFragment.kt
@@ -12,10 +12,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoodot.R
 import com.example.todoodot.data.viewmodel.ToDoViewModel
 import com.example.todoodot.databinding.FragmentListBinding
+import com.example.todoodot.fragments.SharedViewModel
 
 class ListFragment : Fragment() {
 
     private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
     private val adapter: ListAdapter by lazy { ListAdapter() }
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
@@ -33,7 +35,12 @@ class ListFragment : Fragment() {
         val view = binding.root
 
         mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            mSharedViewModel.checkIfDatabaseEmpty(data)
             adapter.setData(data)
+        })
+
+        mSharedViewModel.emptyDatabase.observe(viewLifecycleOwner, Observer {
+            showEmptyDatabaseViews(it)
         })
 
         binding.listOfTodos.adapter = adapter
@@ -48,6 +55,16 @@ class ListFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun showEmptyDatabaseViews(emptyDatabase: Boolean) {
+        if (emptyDatabase) {
+            binding.noDataImageView.visibility = View.VISIBLE
+            binding.noDataTextView.visibility = View.VISIBLE
+        } else {
+            binding.noDataImageView.visibility = View.INVISIBLE
+            binding.noDataTextView.visibility = View.INVISIBLE
+        }
     }
 
     override fun onDestroyView() {
