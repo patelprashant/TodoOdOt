1414ac9f25e2ca5613858cbb440ce97315f6bdd3
Prashant <prashant.patel77@gmail.com>
2021-09-14 23:11:10 +1000

    Added DataBinding and Adapter for List Adapter

--- app/src/main/java/com/example/todoodot/fragments/BindingAdapters.kt
+++ app/src/main/java/com/example/todoodot/fragments/BindingAdapters.kt
@@ -2,12 +2,16 @@ package com.example.todoodot.fragments
 
 import android.view.View
 import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoodot.R
 import com.example.todoodot.data.models.Priority
+import com.example.todoodot.data.models.ToDoData
+import com.example.todoodot.fragments.list.ListFragmentDirections
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BindingAdapters {
@@ -47,9 +51,29 @@ class BindingAdapters {
         fun parsePriorityToInt(view: Spinner, priority: Priority) {
             when (priority) {
                 Priority.HIGH -> view.setSelection(0)
-                Priority.MEDIUM ->  view.setSelection(1)
-                Priority.LOW ->  view.setSelection(2)
+                Priority.MEDIUM -> view.setSelection(1)
+                Priority.LOW -> view.setSelection(2)
             }
+        }
+
+        @BindingAdapter("android:parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(view: CardView, priority: Priority) {
+            when (priority) {
+                Priority.HIGH -> view.setBackgroundColor(view.context.getColor(R.color.purple_700))
+                Priority.MEDIUM -> view.setBackgroundColor(view.context.getColor(R.color.purple_500))
+                Priority.LOW -> view.setBackgroundColor(view.context.getColor(R.color.purple_200))
+            }
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, currentItem: ToDoData){
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+                view.findNavController().navigate(action)
+            }
+
         }
 
     }
--- app/src/main/java/com/example/todoodot/fragments/list/ListAdapter.kt
+++ app/src/main/java/com/example/todoodot/fragments/list/ListAdapter.kt
@@ -2,12 +2,7 @@ package com.example.todoodot.fragments.list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.todoodot.R
-import com.example.todoodot.data.models.Priority
 import com.example.todoodot.data.models.ToDoData
 import com.example.todoodot.databinding.TodoItemLayoutBinding
 
@@ -16,46 +11,55 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     class MyViewHolder(private val itemBinding: TodoItemLayoutBinding) : RecyclerView.ViewHolder(itemBinding.root){
         fun bind(todoItem: ToDoData) {
-            itemBinding.todoTitleText.text = todoItem.title
-            itemBinding.todoDescText.text = todoItem.description
+            itemBinding.toDoDataItem = todoItem
+            itemBinding.executePendingBindings()
 
-            when (todoItem.priority) {
-                Priority.HIGH -> itemBinding.todoPriorityIndicator.setBackgroundColor(
-                    ContextCompat.getColor(
-                        itemView.context,
-                        R.color.purple_700
-                    )
-                )
-                Priority.MEDIUM -> itemBinding.todoPriorityIndicator.setBackgroundColor(
-                    ContextCompat.getColor(
-                        itemView.context,
-                        R.color.purple_500
-                    )
-                )
-                else -> itemBinding.todoPriorityIndicator.setBackgroundColor(
-                    ContextCompat.getColor(
-                        itemView.context,
-                        R.color.purple_200
-                    )
-                )
+
+//            itemBinding.todoTitleText.text = todoItem.title
+//            itemBinding.todoDescText.text = todoItem.description
+
+//            when (todoItem.priority) {
+//                Priority.HIGH -> itemBinding.todoPriorityIndicator.setBackgroundColor(
+//                    ContextCompat.getColor(
+//                        itemView.context,
+//                        R.color.purple_700
+//                    )
+//                )
+//                Priority.MEDIUM -> itemBinding.todoPriorityIndicator.setBackgroundColor(
+//                    ContextCompat.getColor(
+//                        itemView.context,
+//                        R.color.purple_500
+//                    )
+//                )
+//                else -> itemBinding.todoPriorityIndicator.setBackgroundColor(
+//                    ContextCompat.getColor(
+//                        itemView.context,
+//                        R.color.purple_200
+//                    )
+//                )
+//            }
+        }
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder{
+                val itemBinding = TodoItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return MyViewHolder(itemBinding)
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemBinding = TodoItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyViewHolder(itemBinding)
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val toDoItem = dataList[position]
-        holder.bind(toDoItem)
+        val currentDataItem = dataList[position]
+        holder.bind(currentDataItem)
 
-        val rowBackground = holder.itemView.findViewById<ConstraintLayout>(R.id.item_background)
-        rowBackground.setOnClickListener {
-            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(toDoItem)
-            holder.itemView.findNavController().navigate(action)
-        }
+//        val rowBackground = holder.itemView.findViewById<ConstraintLayout>(R.id.item_background)
+//        rowBackground.setOnClickListener {
+//            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentDataItem)
+//            holder.itemView.findNavController().navigate(action)
+//        }
 
     }
 
